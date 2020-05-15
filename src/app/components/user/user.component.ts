ad1848a742108ea0d79257d5c63eb7424cb6ff11
Akrem Karoui <AkremKaroui97@gmail.com>
2020-05-15 20:49:00 +0100

    user model and component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-user',
-  template: '<h2>Akrem Karoui 😎</h2>'
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
 })
 
 export class UserComponent{
+  //props
+  user: User;
+  //methods
+  constructor(){
+    this.user = {
+      firstName: 'Akrem',
+      lastName: 'Karoui',
+      age: 22,
+      address:{
+        state: 'WA',
+        street: '50 Hallgh St',
+        city: 'Seattle'
+      }
+    };
+  }
 
 }
---
+++ src/app/models/User.ts
@@ -0,0 +1,10 @@
+export class User{
+  firstName: String;
+  lastName: String;
+  age: Number;
+  address: {
+    street: String,
+    city: String,
+    state: String,
+  }
+}
